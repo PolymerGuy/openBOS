059f58ba52e3a29cf42438926ba62c71fe5fd55e
PolymerGuy <sindre.n.olufsen@ntnu.no>
2019-03-03 15:32:53 +0100

    New ImageStack definition New ImageStack factories Renaming the submodule

--- Examples/diffBOS_user_imgs.py
+++ Examples/diffBOS_user_imgs.py
@@ -5,10 +5,10 @@ import openBOS as bos
 path_to_img_folder = r"./images/"
 
 # Generate an image stack from all images in the folder
-image_stack = bos.IO.ImageStackFromFolder(path_to_img_folder, file_suffix=".tif", lazy=True)
+image_stack = bos.imageStack.imagestack_from_folder(path_to_img_folder, file_suffix=".tif", lazy=False)
 
 # Remove background drift by high-pass filtering the image_stack
-image_stack.set_filter(bos.filtering.highpass_gaussian)
+# image_stack.set_filter(bos.filtering.highpass_gaussian)
 
 # Define the settings for the analysis
 settings = bos.diffBOS.Settings(First_img_id=0,
